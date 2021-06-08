90add34b5c6fdeca21d263682beda1a543e5c0cd
Edward Silverton <e.silverton@gmail.com>
2021-06-08 12:12:06 +0100

    camera controls

--- stories/ModelViewer.js
+++ stories/ModelViewer.js
@@ -1,21 +1,29 @@
 import './model-viewer.css';
 
 export const createViewer = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  onClick,
+  src,
+  cameraControls,
 }) => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = label;
-  btn.addEventListener('click', onClick);
 
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  btn.className = ['storybook-button', `storybook-button--${size}`, mode].join(' ');
+  const viewer = document.createElement('model-viewer');
+  viewer.src = src;
+  viewer.style = "width: 400px; height: 300px";
+  
+  if (cameraControls) {
+    viewer.setAttribute('camera-controls', true);
+  }
 
-  btn.style.backgroundColor = backgroundColor;
+  return viewer;
 
-  return btn;
+  // const btn = document.createElement('button');
+  // btn.type = 'button';
+  // btn.innerText = label;
+  // btn.addEventListener('click', onClick);
+
+  // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  // btn.className = ['storybook-button', `storybook-button--${size}`, mode].join(' ');
+
+  // btn.style.backgroundColor = backgroundColor;
+
+  // return btn;
 };
--- stories/ModelViewer.stories.js
+++ stories/ModelViewer.stories.js
@@ -3,41 +3,51 @@ import { createViewer } from './ModelViewer';
 export default {
   title: 'Example/ModelViewer',
   argTypes: {
-    label: { control: 'text' },
-    primary: { control: 'boolean' },
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select', options: ['small', 'medium', 'large'] },
-    },
-    onClick: { action: 'onClick' },
+    src: { control: 'text' },
+    // backgroundColor: { control: 'color' },
+    // size: {
+    //   control: { type: 'select', options: ['small', 'medium', 'large'] },
+    // },
+    // onClick: { action: 'onClick' },
   },
 };
 
 const Template = ({ label, ...args }) => {
   // You can either use a function to create DOM elements or use a plain html string!
   // return `<div>${label}</div>`;
-  return createButton({ label, ...args });
+  return createViewer({ label, ...args });
 };
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Src = Template.bind({});
+Src.args = {
+  src: "https://iiif-3d-assets.netlify.app/gltf/astronaut/_astronaut/Astronaut.glb",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const CameraControls = Template.bind({});
+CameraControls.args = {
+  src: "https://iiif-3d-assets.netlify.app/gltf/astronaut/_astronaut/Astronaut.glb",
+  cameraControls: true,
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
